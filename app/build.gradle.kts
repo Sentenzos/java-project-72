f785e70e4dd3c6ec36b8734f969dac1556ea9eb8
sentenzos <sentenzoss@gmail.com>
2024-04-19 00:06:24 +0300

    junit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,14 +18,20 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("io.javalin:javalin:6.1.3")
     testImplementation("org.slf4j:slf4j-simple:2.0.13")
-//    implementation("com.github.johnrengelman.shadow:com.github.johnrengelman.shadow.gradle.plugin:8.1.1")
-
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
 application {
     mainClass = "hexlet.code.App"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
 }
